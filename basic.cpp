914dabab69561762c8adc64bb4e81fa874054143
udaydas06 <168926406+udaydas06@users.noreply.github.com>
2024-06-05 20:26:14 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Dear Uday\n";
+    cout<<"Welcome To The Coding World\n";
+    cout<<"Keep Enjoying\n";
+    
+    return 0;
+
+}
